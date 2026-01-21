6844c4ba22d23e8f3ff03f43e5eab705c3d9c2cb
ishaansalian <146309100+ishaansalian@users.noreply.github.com>
2026-01-21 15:51:41 -0500

    Add files via upload

--- F446_linux/Core/Src/main.c
+++ F446_linux/Core/Src/main.c
@@ -2,86 +2,56 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * @brief          : Simple button press sends CAN message to computer
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "can.h"
-#include "spi.h"
-#include "usart.h"
 #include "gpio.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-#include "position_sensor.h"
-#include "hw_config.h"
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
-uint8_t tx_buffer[27] = "welcome to binaryupdates!\n\r";
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-CAN_TxHeaderTypeDef TxHeader;
-CAN_RxHeaderTypeDef RxHeader;
 
+// CAN transmit structures
+CAN_TxHeaderTypeDef TxHeader;
 uint32_t TxMailbox;
 uint8_t TxData[8];
-uint8_t RxData[8];
+uint32_t message_counter = 0;
 
-uint8_t count = 0;
-
-
-void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+// Button interrupt callback - runs when button pressed
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_5);
-	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData);
+    if (GPIO_Pin == GPIO_PIN_2)  // Button on PA2
+    {
+        // Prepare message: "BTN" + counter
+        TxData[0] = 'B';
+        TxData[1] = 'T';
+        TxData[2] = 'N';
+        TxData[3] = (message_counter >> 24) & 0xFF;
+        TxData[4] = (message_counter >> 16) & 0xFF;
+        TxData[5] = (message_counter >> 8) & 0xFF;
+        TxData[6] = message_counter & 0xFF;
+        TxData[7] = 0x00;
+        
+        // Send CAN message
+        HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
+        
+        // Blink LED to show button was pressed
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_5);  // LED pin
+        
+        message_counter++;
+    }
 }
 
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
-	if (GPIO_Pin == GPIO_PIN_2){
-		TxData[0] = 100;
-		TxData[1] = 10;
-		HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
-	}
-}
 /* USER CODE END 0 */
 
 /**
@@ -90,48 +60,58 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
   */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_SPI3_Init();
   MX_CAN1_Init();
+  
   /* USER CODE BEGIN 2 */
 
-  HAL_CAN_Start(&hcan1);
+  // Start CAN peripheral
+  if (HAL_CAN_Start(&hcan1) != HAL_OK)
+  {
+      Error_Handler();
+  }
 
-  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+  // Configure TX Header (message format)
+  TxHeader.DLC = 8;                    // 8 bytes of data
+  TxHeader.IDE = CAN_ID_STD;           // Standard 11-bit ID
+  TxHeader.RTR = CAN_RTR_DATA;         // Data frame (not remote request)
+  TxHeader.StdId = 0x100;              // CAN ID = 0x100
+  TxHeader.TransmitGlobalTime = DISABLE;
 
-  TxHeader.DLC = 8;
-//  TxHeader.ExtId = 0;
-  TxHeader.IDE = CAN_ID_STD;
-  TxHeader.RTR = CAN_RTR_DATA;
-  TxHeader.StdId = 0x103;
-//  TxHeader.TransmitGlobalTime = DISABLE;
+  // Initialize message data
+  for (int i = 0; i < 8; i++)
+  {
+      TxData[i] = 0;
+  }
 
-  TxData[0] = 0xf3;
-
-//  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
+  // Send a startup message to show board is alive
+  TxData[0] = 'R';
+  TxData[1] = 'D';
+  TxData[2] = 'Y';  // "RDY" = Ready
+  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
+  
+  // Blink LED to show we're ready
+  for (int i = 0; i < 6; i++)
+  {
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_5);
+      HAL_Delay(100);
+  }
 
   /* USER CODE END 2 */
 
@@ -142,20 +122,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_Delay(100); // wait for 1 sec
-	  uint16_t test = 0;
-	  test = readMagAlphaAngle();
-	  float angle_deg = (test * 360.0f) / ENC_CPR;
-
-	  printf("raw = %u, angle = %u deg\r\n", test, (uint16_t) angle_deg);
-	  TxData[0] = (uint8_t) angle_deg;
-//	  if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
-//	  {
-//	     Error_Handler ();
-//	  }
-//	  HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox);
-//	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_5);
-	  HAL_Delay(100); // wait for 1 se
+    
+    // Main loop does nothing - everything happens in button interrupt
+    HAL_Delay(100);
+    
   }
   /* USER CODE END 3 */
 }
@@ -169,14 +139,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -191,15 +156,11 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Activate the Over-Drive mode
-  */
   if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -214,7 +175,6 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
 /**
@@ -223,28 +183,17 @@ void SystemClock_Config(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
-	  printf("having error \r\n");
+      // Error - blink LED rapidly
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_5);
+      HAL_Delay(50);
   }
-  /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif
